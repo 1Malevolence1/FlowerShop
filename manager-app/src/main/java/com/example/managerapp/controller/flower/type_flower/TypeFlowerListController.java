6c15beb472de30147d7eef5db4241199937ae882
Mr.D <danil36rusdanil@mail.ru>
2024-05-24 14:17:54 +0300

    Сделал вывод на странице списко типов

--- manager-app/src/main/java/com/example/managerapp/controller/flower/type_flower/TypeFlowerListController.java
+++ manager-app/src/main/java/com/example/managerapp/controller/flower/type_flower/TypeFlowerListController.java
@@ -1,7 +1,6 @@
 package com.example.managerapp.controller.flower.type_flower;
 
 
-import com.example.managerapp.client.flower.individual_flower.FlowerClientService;
 import com.example.managerapp.client.flower.type_flower.TypeFlowerClientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("main/type_flower")
+@RequestMapping("main/type_flower/list")
 @Slf4j
-public class TypeFlowerController {
+public class TypeFlowerListController {
 
     private final TypeFlowerClientService typeFlowerClientService;
 
     @Autowired
-    public TypeFlowerController(TypeFlowerClientService typeFlowerClientService) {
+    public TypeFlowerListController(TypeFlowerClientService typeFlowerClientService) {
         this.typeFlowerClientService = typeFlowerClientService;
     }
 
     @GetMapping()
     public String getPageListFlowers(Model model){
-        model.addAttribute("flowers", typeFlowerClientService.allFlowers());
-        return "main/flowers/flowers_list";
+        model.addAttribute("type_flowers", typeFlowerClientService.allTypeFlower());
+        return "main/type_flower/type_flower_list_page";
     }
 
 }
--- manager-app/src/main/java/com/example/managerapp/dto/flower/type_flower/TypeFlower.java
+++ manager-app/src/main/java/com/example/managerapp/dto/flower/type_flower/TypeFlower.java
@@ -1,5 +1,10 @@
 package com.example.managerapp.dto.flower.type_flower;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class TypeFlower {
     private Long id;
     private String type;
