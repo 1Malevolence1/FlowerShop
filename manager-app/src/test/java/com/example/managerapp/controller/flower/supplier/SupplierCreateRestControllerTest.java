054ed25315c4873dd7347319766482041afe4e90
Mr.D <danil36rusdanil@mail.ru>
2024-06-03 13:09:38 +0300

    поменял код для конфигурации клиентов

---
+++ manager-app/src/test/java/com/example/managerapp/controller/flower/supplier/SupplierCreateRestControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.managerapp.controller.flower.supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierCreateRestControllerTest {
+
+
+//    SupplierCreateRestController controller = new SupplierCreateRestController();
+}
